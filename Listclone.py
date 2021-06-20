1db0c2b9d83963c3c9255ffbd44a6b53a5e44cd9
Biky Tirkey <vick.tirkey@gmail.com>
2021-06-20 21:26:18 +0530

    python

---
+++ Listclone.py
@@ -0,0 +1,6 @@
+'''Clone a List'''
+
+Original=[15,20,10,5,21,31]
+Clone_List=[x for x in Original]
+print("Cloned List is :",Clone_List)
+print("Original List is :",Original)
---
+++ listempty.py
@@ -0,0 +1,7 @@
+'''check if the list is empty'''
+
+list=[]
+if not list:
+    print("List is empty")
+else:
+    print("List not empty")
